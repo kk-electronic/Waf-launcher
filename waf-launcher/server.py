53f277bc4d380a324eaf064120503ed79128a116
Jes Andersen <sindal@gmail.com>
2011-03-08 13:02:20 +0100

    Deleted unnecessary files and added them to gitignore

--- waf-launcher/server.py
+++
@@ -1,8 +0,0 @@
-from twisted.web.server import Site
-from twisted.web.static import File
-from twisted.internet import reactor
-
-resource = File('/home/albatros/Projects/waf-launcher/waf-launcher/bin/Debug')
-factory = Site(resource)
-reactor.listenTCP(9998, factory)
-reactor.run()
